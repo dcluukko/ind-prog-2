841d9e4ad49ba62100e7d8ec05a7958dfd1732e7
Dana Luukko <Dcluukko@gmail.com>
2020-01-17 13:08:16 -0800

    It will hide all submissions outside of language options

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,39 @@ $(document).ready(function() {
 		$("#please-fill").show();
 		} else if (qSums <=5 && age >= 15 ){
 			$("#jScript").show();
+			$("#python").hide();
+			$("#rust").hide();
+			$("#swift").hide();
+			$("#outside").hide();
+			$("#please-fill").hide();
 		} else if (qSums >=6 && qSums <=10  && age >= 15) {
 			$("#python").show();
+			$("#rust").hide();
+			$("#swift").hide();
+			$("#outside").hide();
+			$("#please-fill").hide();
+			$("#jScript").hide();
 		} else if (qSums >=11 && qSums <= 15 && age >=15) {
 			$("#rust").show();
+			$("#swift").hide();
+			$("#outside").hide();
+			$("#please-fill").hide();
+			$("#python").hide();
+			$("#jScript").hide();
 		} else if (qSums >= 16 && qSums <= 20 && age >= 15)	{
 			$("#swift").show();
+			$("#outside").hide();
+			$("#please-fill").hide();
+			$("#python").hide();
+			$("#rust").hide();
+			$("#jScript").hide();
 		} else if (age <= 14)	{
 			$("#outside").show();
+			$("#please-fill").hide();
+			$("#jScript").hide();
+			$("#python").hide();
+			$("#rust").hide();
+			$("#swift").hide();
 		} else {
 			
 		}
